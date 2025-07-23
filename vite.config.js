e63b759ea60123fec78877aed45aa09adde984bd
Xenosword-X <swordsgod790626@gmail.com>
2025-07-23 11:05:30 +0800

    修正網站路徑

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-todolist/',
+  base: '/vite-project/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
